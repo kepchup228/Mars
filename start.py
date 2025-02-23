dcca31063bbed60adf7c6948e878fbaf995f1c94
kepchup228 <164914727+kepchup228@users.noreply.github.com>
2025-02-23 23:17:07 +0300

    master

---
+++ start.py
@@ -0,0 +1,51 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/image')
+def PromotionImage():
+    return '''<!DOCTYPE html>
+<html>
+<head>
+    <title>Bootstrap Carousel</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <h1 class="text-success">Пейзажи Марса</h1>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="static/image1.jpg"/>
+            </div>
+            <div class="item">
+                <img src="static/image2.jpg"/>
+            </div>
+            <div class="item">
+                <img src="static/image3.jpg"/>
+            </div>
+        </div>
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</body>
+</html>'''
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
